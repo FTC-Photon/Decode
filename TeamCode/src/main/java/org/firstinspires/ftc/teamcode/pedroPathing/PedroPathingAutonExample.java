5f981ce99a7278f9b0c4e3e63360daa611181979
benson-e <bensonella543@gmail.com>
2025-10-24 22:26:16 -0500

    PedroPathing
    start of pedro auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PedroPathingAutonExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PedroPathingAutonExample.java
@@ -8,12 +8,13 @@ import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
 @Autonomous
-public class PedroPathingAuton extends OpMode {
+public class PedroPathingAutonExample extends OpMode {
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
     private int pathState;
-    private final Pose startPose = new Pose(28.5, 128, Math.toRadians(180)); // Start Pose of our robot.
-    private final Pose scorePose = new Pose(60, 85, Math.toRadians(135)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
+    private final Pose startPose = new Pose(48, 144, Math.toRadians(180)); // Start Pose currently left corner subject to change
+    private final Pose scorePose = new Pose(20, 144, Math.toRadians(135)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
+    //test this seems ok
     private final Pose pickup1Pose = new Pose(37, 121, Math.toRadians(0)); // Highest (First Set) of Artifacts from the Spike Mark.
     private final Pose pickup2Pose = new Pose(43, 130, Math.toRadians(0)); // Middle (Second Set) of Artifacts from the Spike Mark.
     private final Pose pickup3Pose = new Pose(49, 135, Math.toRadians(0)); // Lowest (Third Set) of Artifacts from the Spike Mark.
