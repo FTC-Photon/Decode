df3c5e48d15951800b19b9bc269d5eef62de0b05
benson-e <bensonella543@gmail.com>
2025-12-06 10:22:16 -0600

    update pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PedroPathingCR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PedroPathingCR.java
@@ -12,7 +12,7 @@ import org.firstinspires.ftc.teamcode.Mechanisms.AutoScore;
 import org.firstinspires.ftc.teamcode.Mechanisms.intake;
 
 @Autonomous
-public class PedroPathingAutonExample extends OpMode {
+public class pedroPathingCR extends OpMode {
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
     private int pathState;
@@ -74,7 +74,7 @@ public class PedroPathingAutonExample extends OpMode {
                 .build();
     }
 
-    public void autonomousPathUpdate() {
+    public void autonomousPathUpdate() throws InterruptedException {
         switch (pathState) {
             case 0:
                 follower.followPath(scorePreload);
@@ -91,7 +91,7 @@ public class PedroPathingAutonExample extends OpMode {
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
                     /* Score Preload */
-                    autoScore.AutonScore(1,1,500);
+                    autoScore.AutonScore(1,1,1,500);
                     /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     follower.followPath(grabPickup1,true);
                     setPathState(2);
@@ -135,7 +135,7 @@ public class PedroPathingAutonExample extends OpMode {
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
                     /* Score Sample */
-                    autoScore.AutonScore(1,1,500);
+                    autoScore.AutonScore(1,1,1,500);
                     /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     follower.followPath(grabPickup3,true);
                     setPathState(6);
@@ -173,7 +173,11 @@ public class PedroPathingAutonExample extends OpMode {
 
         // These loop the movements of the robot, these must be called continuously in order to work
         follower.update();
-        autonomousPathUpdate();
+        try {
+            autonomousPathUpdate();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
 
         // Feedback to Driver Hub for debugging
