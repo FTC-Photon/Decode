53c8367facc56d06bf8c7b066001607450943a9f
benson-e <bensonella543@gmail.com>
2026-01-20 18:29:43 -0600

    MT1 and 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLimelightMT1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLimelightMT1.java
@@ -1,23 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
-import com.pedropathing.ftc.FTCCoordinates;
-import com.pedropathing.geometry.PedroCoordinates;
-import com.pedropathing.geometry.Pose;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-@TeleOp
-public class AprilTagLimelight extends OpMode{
+
+@Autonomous
+public class AprilTagLimelightMT1 extends OpMode{
     private Limelight3A limelight;
     private IMU imu;
 
@@ -31,17 +25,18 @@ public class AprilTagLimelight extends OpMode{
         RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
         imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
     }
-
+    @Override
+    public void start(){
+        limelight.start();
+    }
 
     public void loop(){
         LLResult result = limelight.getLatestResult();
         if (result != null && result.isValid()) {
             Pose3D botpose = result.getBotpose();
-            if (botpose != null) {
                 double x = botpose.getPosition().x;
                 double y = botpose.getPosition().y;
                 telemetry.addData("MT1 Location", "(" + x + ", " + y + ")");
-            }
         }
     }
 
